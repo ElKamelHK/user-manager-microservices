fd24c4d91810dd749983060d10bb50c8488ae831
ElKamelHK <hamdiek58@gmail.com>
2021-06-29 16:47:25 +0100

    user manger microservices

---
+++ src/main/java/com/sha/microserviceusermangements/MicroserviceUserMangementsApplication.java
@@ -0,0 +1,13 @@
+package com.sha.microserviceusermangements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUserMangementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUserMangementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sha/microserviceusermangements/MicroserviceUserMangementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.microserviceusermangements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserMangementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
